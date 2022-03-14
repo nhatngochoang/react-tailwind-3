e4199a332c51805ad89785ee3ebce4197ca40b5c
tahn-0102 <ongthoxunu@gmail.com>
2022-03-14 22:00:20 +0700

    Update Banner.jsx

--- src/portfolio/components/Banner.jsx
+++ src/portfolio/components/Banner.jsx
@@ -16,7 +16,7 @@ const Banner = () => {
             <div className="max-w-[300px] my-0 mx-auto text-center text-gray-var md:text-left md:m-0 md:p-0 md:max-w-[600px]">
                <h1 className="fs-[36px] pt-[60vh] mb-[28px] md:pt-0">Hello, I'm Nhat</h1>
                <p className="banner__heading__para">
-                  As a web developer with a passion for JavaScript, React...Maybe Node or Native in the future :))
+                  As a web developer with a passion for JavaScript, React... Maybe Node or Native in the future :))
                </p>
                <p className="banner__heading__para"> I've been constantly learning technical knowledge. I really love solving real world problems with JS and use different tools...  </p>
                <a href="#viewmore"><button className="banner__btn">View More</button></a>

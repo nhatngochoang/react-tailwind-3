19c2f6d38baa6f4a87e7c807e922071ce1ee9109
tahn-0102 <ongthoxunu@gmail.com>
2022-03-15 19:08:34 +0700

    update aos scroll

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App.js';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { gsap } from "gsap";
+import { PixiPlugin } from "gsap/PixiPlugin.js";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin.js";
+
+//without this line, PixiPlugin and MotionPathPlugin may get dropped by your bundler (tree shaking)...
+gsap.registerPlugin(PixiPlugin, MotionPathPlugin);
 
 ReactDOM.render(
    <React.StrictMode>

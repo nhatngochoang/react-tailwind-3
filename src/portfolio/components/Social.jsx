91520b6cdc4f3ed8f9624ef87da4d0f08b002188
tahn-0102 <ongthoxunu@gmail.com>
2022-06-09 18:37:54 +0700

    Update Social.jsx

--- src/portfolio/components/Social.jsx
+++ src/portfolio/components/Social.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Social = () => {
    return (<>
       <div className="mt-2 pt-10 flex justify-evenly md:justify-start">
-         <a className="text-gray-var hover:text-green-l-var smooth md:mr-[45px]" href="https://github.com/tahn-0102"><FontAwesomeIcon icon={faGithub} size='2x' /></a>
+         <a className="text-gray-var hover:text-green-l-var smooth md:mr-[45px]" href="https://github.com/nhatngochoang"><FontAwesomeIcon icon={faGithub} size='2x' /></a>
          <a className="text-gray-var hover:text-green-l-var smooth md:mr-[45px]" href="https://www.linkedin.com/in/nhat-hoang-4704a1230/"><FontAwesomeIcon icon={faLinkedin} size='2x' /></a>
          <a className="text-gray-var hover:text-green-l-var smooth md:mr-[45px]" href="https://www.facebook.com/profile.php?id=100008695002406"><FontAwesomeIcon icon={faFacebook} size='2x' /></a>
       </div>
